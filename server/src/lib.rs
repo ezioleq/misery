7f8e33e2ef001b62b5fece9664e2a9c9cb8e790e
ezioleq <git@ezioleq.com>
2024-11-02 00:07:06 +0100

    Untidy server pong implementation

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,1 +1,55 @@
-pub fn start_server() {}
+use log::{debug, info};
+use protocol::packet::{DisconnectKickPacket, Packet, ToBytes};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
+
+pub async fn start_server() {
+    info!("Hello! :3");
+    let listener = TcpListener::bind("127.0.0.1:25565").await.unwrap();
+
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        debug!("Connection from {:?}", &addr);
+
+        tokio::spawn(async move {
+            let mut buf = vec![0u8; 1024];
+
+            loop {
+                let n = socket
+                    .read(&mut buf)
+                    .await
+                    .expect("Failed to read data from socket");
+
+                if n == 0 {
+                    return;
+                }
+
+                debug!("Received packet: {:?}", buf);
+
+                let packet = Packet::try_from(&buf[..]).unwrap();
+
+                match packet {
+                    Packet::ServerListPing(_) => {
+                        debug!("Received server ping packet!");
+
+                        let packet = DisconnectKickPacket {
+                            reason: "A Minecraft Server§0§20".to_string(),
+                        }
+                        .to_bytes()
+                        .unwrap();
+
+                        debug!("Sending status packet: {:?}", packet.as_ref() as &[u8]);
+
+                        socket
+                            .write_all(packet.as_ref())
+                            .await
+                            .expect("Failed to write data to socket");
+                    }
+                    _ => panic!("uh nuh"),
+                }
+            }
+        });
+    }
+}
